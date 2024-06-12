9d61fa0cf31477f4876e67d19641de92014809bc
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-06-12 12:20:29 +0300

    Task 3 in Basic level

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,7 @@ changeTextBtn.onclick = (e) => {
   changeTextBtn.style.color = `green`;
 };
 
+console.log(`Done`);
 console.log(`Task 2`);
 
 const changeColorBtn = document.querySelector(`.changeColorBtn`);
@@ -19,3 +20,15 @@ const changeColorBtn = document.querySelector(`.changeColorBtn`);
 changeColorBtn.onclick = (e) => {
   changeColorBtn.style.backgroundColor = `orange`;
 };
+
+console.log(`Done`);
+console.log(`Task 3`);
+
+const patrickImg = document.querySelector(`.patrickImg`);
+
+function changeImg(event) {
+  patrickImg.src = `https://www.inspireuplift.com/resizer/?image=https://cdn.inspireuplift.com/uploads/images/seller_products/30041/1703153928_Spongebob-40.png&width=600&height=600&quality=90&format=auto&fit=pad`;
+  patrickImg.alt = `Patrick with a ball`;
+}
+
+patrickImg.onmouseenter = changeImg;
